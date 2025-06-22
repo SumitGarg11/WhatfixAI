f7f03edd90ef75c5d69771be8a793721982992d0
SumitGarg <garg10sumit@gmail.com>
2025-06-23 04:05:39 +0530

    feat: implement Chain class for job parsing and cold email generation using Groq LLM

---
+++ app/chains.py
@@ -0,0 +1,57 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Mohan, a Business Development Executive at Whatfix. Whatfix is a leading Digital Adoption Platform that empowers organizations to simplify user onboarding, training, and software adoption through in-app guidance and automation.  
+            Over the years, Whatfix has helped global enterprises enhance user productivity, reduce support costs, and accelerate digital transformation.  
+            Your job is to write a personalized cold email to the client regarding the job mentioned above, highlighting how Whatfix can address their onboarding and training challenges for this role.  
+            Also add the most relevant ones from the following links to showcase Whatfix's portfolio and success stories: {link_list}  
+            Remember, you are Mohan, BDE at Whatfix.  
+            Do not provide a preamble.  
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
